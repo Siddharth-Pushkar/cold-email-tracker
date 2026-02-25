57bfaf3e863cefc6d46547ec79648981faefd374
Siddharth-Pushkar <siddharthpushkar04@gmail.com>
2026-02-25 13:50:19 +0530

    enviorment changes 3

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Replace the values below with your Firebase project's config.
+// Get this from Firebase Console -> Project settings -> Your apps
+export const firebaseConfig = {
+  apiKey: "FB_API_KEY",
+  authDomain: "FB_AUTH_DOMAIN",
+  projectId: "FB_PROJECT_ID",
+  storageBucket: "FB_STORAGE_BUCKET",
+  messagingSenderId: "FB_MESSAGING_SENDER_ID",
+  appId: "FB_APP_ID",
+  measurementId: "FB_MEASUREMENT_ID"
+};
